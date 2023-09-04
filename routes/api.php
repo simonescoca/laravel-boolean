0d5dd0347deea704e1d02a248d97850d3fc63f8a
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-09-04 17:52:52 +0200

    added routes path

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\CockailController as ApiCockailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+
+Route::get('/cocktails', [ApiCockailController::class, 'index'])->name('guest.cocktails.index');
+Route::get('/cocktails/{cocktail}', [ ApiCockailController::class, 'show' ])->name('guest.cocktails.show');
